24565abdce7530227ce94a9bc64af82711e0fce9
Fea <radidani55@gmail.com>
2022-08-25 12:50:12 +0200

    stuff

---
+++ 08.25/project/main.py
@@ -0,0 +1,104 @@
+matrix = [0] * 8
+
+for x in range(len(matrix)):
+    matrix[x] = [(0, " ")] * 8
+
+
+def display():
+    for i, line in enumerate(matrix):
+        for piece in line:
+            player, piece_id = piece
+            if player == 1:
+                print(w_chess.get(piece_id, '　').center(3), end="")
+            else:
+                print(b_chess.get(piece_id, '　').center(3), end="")
+        print(8 - i)
+    chars = 'ａｂｃｄｅｆｇｈ'
+    for each in chars:
+        print(each.center(3), end='')
+    print()
+
+
+order = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
+
+#       player id, piece id
+piece = (0, 0)
+
+
+def start_pos():
+    for i in range(len(order)):
+        matrix[7][i] = (1, order[i])
+        matrix[6][i] = (1, 'pawn')
+        matrix[0][i] = (2, order[i])
+        matrix[1][i] = (2, 'pawn')
+
+
+b_chess = {
+    'king': '♔',
+    'queen': '♕',
+    'rook': '♖',
+    'bishop': '♗',
+    'knight': '♘',
+    'pawn': '♙'
+}
+w_chess = {
+    'king': '♚',
+    'queen': '♛',
+    'rook': '♜',
+    'bishop': '♝',
+    'knight': '♞',
+    'pawn': '♟'
+}
+
+
+def check_valid_move(chosen_piece, from_location, target_location):
+    match chosen_piece:
+        case 'pawn':
+            if from_location[1] == 7 or from_location[1] == 2:
+                if target_location[1]
+        case 'knight':
+            pass
+        case 'bishop':
+            pass
+        case 'rook':
+            pass
+        case 'queen':
+            pass
+        case 'king':
+            pass
+
+
+def main():
+    current_player = 1
+    start_pos()
+
+    while True:
+        print(f"Current player: {'white' if current_player == 1 else 'black'}")
+
+        display()
+
+        while True:
+            from_location = input('Choose piece to move:  ')
+            letters = 'abcdefgh'
+            if from_location[0] in letters and 0 < int(from_location[1]) < 9 and len(from_location) == 2:
+                l_index = letters.find(from_location[0])
+                if matrix[8 - int(from_location[1])][l_index][0] == current_player:
+                    chosen_piece = matrix[8 - int(from_location[1])][l_index][1]
+                    break
+
+        while True:
+            target_location = input('Where to move piece:  ')
+            letters = 'abcdefgh'
+            if target_location[0] in letters and 0 < int(target_location[1]) < 9 and len(target_location) == 2:
+                l_index = letters.find(target_location[0])
+                is_occupied = matrix[8 - int(target_location[1])][l_index][0]
+                what_is_there = matrix[8 - int(target_location[1])][l_index][1]
+                check_valid_move(chosen_piece, from_location, target_location)
+
+        print(from_location, 'is', matrix[8 - int(from_location[1])][l_index][1])
+
+        current_player = int(not bool(current_player - 1)) + 1
+
+
+if __name__ == '__main__':
+    main()
